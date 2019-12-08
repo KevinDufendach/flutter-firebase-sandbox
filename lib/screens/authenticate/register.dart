8cb467fa9c3e540aaf849ee81ff3664831d6e032
Kevin Dufendach <kevin.dufendach@gmail.com>
2019-12-08 14:14:25 -0500

    allow registration and authorization

--- lib/screens/authenticate/register.dart
+++ lib/screens/authenticate/register.dart
@@ -2,9 +2,9 @@ import 'package:flutter/material.dart';
 import 'package:flutter_firebase_sandbox/services/auth.dart';
 
 class Register extends StatefulWidget {
-
   final Function toggleView;
-  Register({ this.toggleView });
+
+  Register({this.toggleView});
 
   @override
   _RegisterState createState() => _RegisterState();
@@ -12,6 +12,7 @@ class Register extends StatefulWidget {
 
 class _RegisterState extends State<Register> {
   final AuthService _authSvc = AuthService();
+  final _formKey = GlobalKey<FormState>();
 
   // text field state
   String email = '';
@@ -38,36 +39,40 @@ class _RegisterState extends State<Register> {
       body: Container(
         padding: EdgeInsets.symmetric(vertical: 20.0, horizontal: 50.0),
         child: Form(
-            child: Column(
-          children: <Widget>[
-            SizedBox(height: 20.0),
-            TextFormField(
-              onChanged: (val) {
-                setState(() => email = val);
-              },
-            ),
-            SizedBox(height: 20.0),
-            TextFormField(
-              obscureText: true,
-              onChanged: (val) {
-                setState(() => password = val);
-              },
-            ),
-            SizedBox(height: 20.0),
-            RaisedButton(
-              color: Colors.pink[400],
-              child: Text(
-                'Register',
-                style: TextStyle(color: Colors.white),
+          key: _formKey,
+          child: Column(
+            children: <Widget>[
+              SizedBox(height: 20.0),
+              TextFormField(
+                validator: (val) => val.isEmpty ? 'Enter email' : null,
+                onChanged: (val) {
+                  setState(() => email = val);
+                },
               ),
-              onPressed: () async {
-                print(email);
-                print(password);
-//                _authSvc.signInAnon();
-              },
-            ),
-          ],
-        )),
+              SizedBox(height: 20.0),
+              TextFormField(
+                obscureText: true,
+                validator: (val) => val.length < 6 ? 'Enter a password with 6+ characters' : null, // return null when valid
+                onChanged: (val) {
+                  setState(() => password = val);
+                },
+              ),
+              SizedBox(height: 20.0),
+              RaisedButton(
+                color: Colors.pink[400],
+                child: Text(
+                  'Register',
+                  style: TextStyle(color: Colors.white),
+                ),
+                onPressed: () async {
+                  if (_formKey.currentState.validate()) {
+                    _authSvc.registerWithEmailAndPassword(email, password);
+                  }
+                },
+              ),
+            ],
+          ),
+        ),
       ),
     );
   }
